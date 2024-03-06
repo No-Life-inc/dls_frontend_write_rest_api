200ff15787bcb8bef722a01d4d3128e4258ac070
Morten Bendeke <mbendeke@hotmail.com>
2024-03-06 14:47:01 +0100

    connected to docker mssql

---
+++ app/db/db-config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    user: 'SA',
+    password: 'YourStrongPassword123',
+    server: 'localhost', // You might need to replace this with your Docker container's IP
+    database: 'mssqlWrite',
+    options: {
+        encrypt: false
+      }
+  };
--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import sql from 'mssql';
+import {config} from './db/db-config';
 
 const app = express();
 const port = 3000;
+
+
+// Connect to the database
+sql.connect(config).then(() => {
+    console.log('Connected to the database.');
+  }).catch(err => {
+    console.error('Error connecting to the database: ', err);
+  });
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
