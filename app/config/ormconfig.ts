0b623f8957a92562bd50a675490ee2ddae538750
Morten Bendeke <mbendeke@hotmail.com>
2024-05-14 21:31:59 +0200

    docker container works

--- app/config/ormconfig.ts
+++ app/config/ormconfig.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 const connectDB = new DataSource({
     type: "mssql",
-    host: process.env.MSSERVER,
-    port: parseInt(process.env.DB_PORT || "1433"),
-    username: process.env.MSUSER,
-    password: process.env.MSPW,
-    database: process.env.MSDB,
+    host: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_USER || "1433"),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_FRONTEND,
     options: {
         encrypt: true,
         trustServerCertificate: true
